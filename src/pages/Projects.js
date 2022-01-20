736bb36c865c1c73288941c25086d4b2fd8464eb
Charles NDAYISABA <nccharles1@gmail.com>
2022-01-20 18:22:13 +0200

    first commit

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -27,121 +27,14 @@ import { LoadingButton } from '@mui/lab';
 import { ProjectCard } from '../components/home';
 import { BasicModal } from '../components/_dashboard/projects';
 import Page from '../components/Page';
-// import Label from '../components/Label';
-// import Scrollbar from '../components/Scrollbar';
-// import SearchNotFound from '../components/SearchNotFound';
-// import { UserListHead, UserListToolbar, UserMoreMenu } from '../components/_dashboard/user';
-//
-// import USERLIST from '../_mocks_/user';
+
 import { projectsMock } from '../__mocks__/projectsMock';
-// import projectsData from '../_mocks_/projects';
-// import { projectsData } from '../__mocks__/projects';
 
-// ----------------------------------------------------------------------
-
-// const TABLE_HEAD = [
-//   { id: 'name', label: 'Project Name', alignRight: false },
-//   { id: 'description', label: 'Project Description', alignRight: false },
-//   { id: 'company', label: 'Company', alignRight: false },
-//   { id: 'clients', label: 'Clients', alignRight: false },
-//   { id: 'status', label: 'Status', alignRight: false },
-//   { id: '' }
-// ];
-
-// ----------------------------------------------------------------------
-
-// function descendingComparator(a, b, orderBy) {
-//   if (b[orderBy] < a[orderBy]) {
-//     return -1;
-//   }
-//   if (b[orderBy] > a[orderBy]) {
-//     return 1;
-//   }
-//   return 0;
-// }
-
-// function getComparator(order, orderBy) {
-//   return order === 'desc'
-//     ? (a, b) => descendingComparator(a, b, orderBy)
-//     : (a, b) => -descendingComparator(a, b, orderBy);
-// }
-
-// function applySortFilter(array, comparator, query) {
-//   const stabilizedThis = array.map((el, index) => [el, index]);
-//   stabilizedThis.sort((a, b) => {
-//     const order = comparator(a[0], b[0]);
-//     if (order !== 0) return order;
-//     return a[1] - b[1];
-//   });
-//   if (query) {
-//     return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
-//   }
-//   return stabilizedThis.map((el) => el[0]);
-// }
 
 export default function Projects() {
-  // const [page, setPage] = useState(0);
-  // const [order, setOrder] = useState('asc');
-  // const [selected, setSelected] = useState([]);
-  // const [orderBy, setOrderBy] = useState('name');
-  // const [filterName, setFilterName] = useState('');
-  // const [rowsPerPage, setRowsPerPage] = useState(5);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
-  // const handleRequestSort = (event, property) => {
-  //   const isAsc = orderBy === property && order === 'asc';
-  //   setOrder(isAsc ? 'desc' : 'asc');
-  //   setOrderBy(property);
-  // };
-
-  // const handleSelectAllClick = (event) => {
-  //   if (event.target.checked) {
-  //     const newSelecteds = USERLIST.map((n) => n.name);
-  //     setSelected(newSelecteds);
-  //     return;
-  //   }
-  //   setSelected([]);
-  // };
-
-  // const handleClick = (event, name) => {
-  //   const selectedIndex = selected.indexOf(name);
-  //   let newSelected = [];
-  //   if (selectedIndex === -1) {
-  //     newSelected = newSelected.concat(selected, name);
-  //   } else if (selectedIndex === 0) {
-  //     newSelected = newSelected.concat(selected.slice(1));
-  //   } else if (selectedIndex === selected.length - 1) {
-  //     newSelected = newSelected.concat(selected.slice(0, -1));
-  //   } else if (selectedIndex > 0) {
-  //     newSelected = newSelected.concat(
-  //       selected.slice(0, selectedIndex),
-  //       selected.slice(selectedIndex + 1)
-  //     );
-  //   }
-  //   setSelected(newSelected);
-  // };
-
-  // const handleChangePage = (event, newPage) => {
-  //   setPage(newPage);
-  // };
-
-  // const handleChangeRowsPerPage = (event) => {
-  //   setRowsPerPage(parseInt(event.target.value, 10));
-  //   setPage(0);
-  // };
-
-  // const handleFilterByName = (event) => {
-  //   setFilterName(event.target.value);
-  // };
-
-  // const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
-
-  // const filteredUsers = applySortFilter(USERLIST, getComparator(order, orderBy), filterName);
-
-  // const isUserNotFound = filteredUsers.length === 0;
-
   return (
     <Page title="Projects | Issue Tracker">
       <Container>
