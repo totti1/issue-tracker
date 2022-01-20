ffc8556436998431287e3a2c267334a7456af657
Aristote Katy <stotestorch@gmail.com>
2022-01-20 02:05:16 +0200

    integrated some backend

--- src/components/authentication/login/LoginForm.js
+++ src/components/authentication/login/LoginForm.js
@@ -4,7 +4,7 @@ import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { useFormik, Form, FormikProvider } from 'formik';
 import { Icon } from '@iconify/react';
 import eyeFill from '@iconify/icons-eva/eye-fill';
-import axios from 'axios';
+// import axios from 'axios';
 import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
 // material
 import {
@@ -19,6 +19,8 @@ import {
 import { LoadingButton } from '@mui/lab';
 
 // ----------------------------------------------------------------------
+const axios = require('axios');
+
 const API =
   process.env.NODE_ENV !== 'production'
     ? process.env.REACT_APP_API_DEV
